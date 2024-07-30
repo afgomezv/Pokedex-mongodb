81e2d564acd8eae232821c50ef9929a09296aae0
afgomezv <afgomezvonline@gmail.com>
2024-07-29 20:58:48 -0500

    deploy railway

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,5 +1,7 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
 import { isValidObjectId, Model } from 'mongoose';
 
 import { Pokemon } from './entities/pokemon.entity';
@@ -11,17 +13,19 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 
 
+
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number;
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ){
-    console.log(process.env.DEFAULT_LIMIT);
-    
-  }
+    private readonly pokemonModel: Model<Pokemon>,
 
+    private readonly configService: ConfigService
+  ){
+    this.defaultLimit = configService.get<number>('defaultLimit')
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase()
@@ -36,7 +40,7 @@ export class PokemonService {
   }
 
   async findAll(paginationDto: PaginationDto) {
-    const {limit= 5, offset= 0} = paginationDto
+    const {limit= this.defaultLimit, offset= 0} = paginationDto
 
     const pokemons = await this.pokemonModel.find()
       .limit(limit)
